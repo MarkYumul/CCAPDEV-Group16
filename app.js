ed041c25d1d696532f328c6a7bddd89b32781478
MarkYumul <125226288+MarkYumul@users.noreply.github.com>
2024-04-05 23:46:44 +0800

    commit  again

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ server.engine('hbs', handlebars.engine({
 server.use(express.static('public'));
 
 const mongoose = require('mongoose');
-const mongo_uri = 'mongodb://127.0.0.1:27017/r_colludium';
+const mongo_uri = 'mongodb+srv://markisaiahyumul:fishy@r-colludium.4xkevb6.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongo_uri);
 
 const bcrypt = require('bcrypt');
